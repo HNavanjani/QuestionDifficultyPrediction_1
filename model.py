15f5c1dd8f4827926f89ffdb6f0418e18acf1bdc
AlphaSpirits <kisalkan@opusxenta.com>
2020-07-06 21:31:52 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,40 @@
+# Import dependencies
+import pandas as pd
+import numpy as np
+
+# Load the dataset in a dataframe object and include only 3 features as mentioned
+url = "C://CDAP_API//modelK_1//PlayerData_1.csv"
+df = pd.read_csv(url)
+include = ['scoreLevel', 'timeLevel', 'difficultyLevel'] # Only 3 features
+df_ = df[include]
+
+# Data Preprocessing
+categoricals = []
+for col, col_type in df_.dtypes.iteritems():
+     if col_type == 'O':
+          categoricals.append(col)
+     else:
+          df_[col].fillna(0, inplace=True)
+
+df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
+
+# Logistic Regression classifier
+from sklearn.linear_model import LogisticRegression
+dependent_variable = 'difficultyLevel'
+x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+y = df_ohe[dependent_variable]
+lr = LogisticRegression()
+lr.fit(x, y)
+
+# Save your model
+from sklearn.externals import joblib
+joblib.dump(lr, 'model.pkl')
+print("Model dumped!")
+
+# Load the model that you just saved
+lr = joblib.load('model.pkl')
+
+# Saving the data columns from training
+model_columns = list(x.columns)
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
